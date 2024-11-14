4a857e6cf9c828df9a854a6cf2547d52dc664070
MoniMahmuda <monimahmuda52@gmail.com>
2024-11-15 00:46:36 +0600

    eighth commit

---
+++ market_manager.js
@@ -0,0 +1,9 @@
+function displayManagerInfo() {
+    
+    const managerId = document.getElementById("managerId").value;
+    const managerName = document.getElementById("managerName").value;
+
+    
+    document.getElementById("displayManagerId").innerText = managerId;
+    
+}
